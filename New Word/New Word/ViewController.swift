7be81e352313113fecc058ffd4405567b4cee433
Arajlym Kabykenova <arailym.kabykenova@icloud.com>
2025-12-16 19:07:33 +0500

    first screen on strb

---
+++ New Word/New Word/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New Word
+//
+//  Created by Арайлым Кабыкенова on 15.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
